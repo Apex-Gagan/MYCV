fc7543eb24e3b439a8eb0ceb446f5b9515995baf
Apex-Gagan <=>
2026-09-21 16:48:12 +0530

    new

--- gagan_Cv/settings.py
+++ gagan_Cv/settings.py
@@ -93,6 +93,9 @@ if os.getenv("DATABASE_URL"):
     DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
 
 DATABASE_URL = (
     os.getenv("NEON_DB_DATABASE_URL")
